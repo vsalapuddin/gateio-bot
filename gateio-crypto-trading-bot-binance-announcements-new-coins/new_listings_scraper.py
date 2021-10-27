b15c2f3070f00fb3453cdbe3eb93749e3f6fd287
Vince Salapuddin <vsalapuddin@ufl.edu>
2021-10-27 02:47:50 -0400

    Add files via upload

---
+++ gateio-crypto-trading-bot-binance-announcements-new-coins/new_listings_scraper.py
@@ -0,0 +1,106 @@
+import ast
+import os.path
+import re
+import time
+import re
+
+import requests
+from gate_api import ApiClient, SpotApi
+
+from auth.gateio_auth import *
+from logger import logger
+from store_order import *
+
+client = load_gateio_creds('auth/auth.yml')
+spot_api = SpotApi(ApiClient(client))
+
+global supported_currencies
+
+def get_last_coin():
+    """
+    Scrapes new listings page for and returns new Symbol when appropriate
+    """
+    logger.debug("Pulling announcement page")
+    latest_announcement = requests.get("https://www.binance.com/bapi/composite/v1/public/cms/article/catalog/list/query?catalogId=48&pageNo=1&pageSize=15&rnd=" + str(time.time()))
+    latest_announcement = latest_announcement.json()
+    logger.debug("Finished pulling announcement page")
+    latest_announcement = latest_announcement['data']['articles'][0]['title']
+
+    found_coin = re.findall('\(([^)]+)', latest_announcement)
+
+    uppers = None
+
+
+
+    exclusions = ['Futures', 'Margin', 'adds', 'Adds']
+    for item in exclusions:
+        if item in latest_announcement:
+            return None
+        else:
+            if len(found_coin) == 1:
+                uppers = found_coin[0]
+                logger.info('New coin detected: ' + uppers)
+            if len(found_coin) != 1:
+                uppers = None
+
+    return uppers
+
+
+def store_new_listing(listing):
+    """
+    Only store a new listing if different from existing value
+    """
+
+    if os.path.isfile('new_listing.json'):
+        file = load_order('new_listing.json')
+        if listing in file:
+            return file
+        else:
+            file = listing
+            store_order('new_listing.json', file)
+            logger.info("New listing detected, updating file")
+            return file
+    else:
+        new_listing = store_order('new_listing.json', listing)
+        logger.info("File does not exist, creating file")
+
+        return new_listing
+
+
+def search_and_update():
+    """
+    Pretty much our main func
+    """
+    while True:
+        time.sleep(3)
+        try:
+            latest_coin = get_last_coin()
+            if latest_coin:
+                store_new_listing(latest_coin)
+            logger.info("Checking for coin announcements every 1 minute (in a separate "
+                       "thread)")
+        except Exception as e:
+            logger.info(e)
+    else:
+        logger.info("while True loop in search_and_update has stopped.")
+
+
+def get_all_currencies(single=False):
+    """
+    Get a list of all currencies supported on gate io
+    :return:
+    """
+    global supported_currencies
+    while True:
+        logger.info("Getting the list of supported currencies from gate io")
+        all_currencies = ast.literal_eval(str(spot_api.list_currencies()))
+        currency_list = [currency['currency'] for currency in all_currencies]
+        with open('currencies.json', 'w') as f:
+            json.dump(currency_list, f, indent=4)
+            logger.info("List of gate io currencies saved to currencies.json. Waiting 5 "
+                  "minutes before refreshing list...")
+        supported_currencies = currency_list
+        if single:
+            return supported_currencies
+        else:
+            time.sleep(300)
---
+++ gateio-crypto-trading-bot-binance-announcements-new-coins/trade_client.py
@@ -0,0 +1,46 @@
+from logger import logger
+
+from auth.gateio_auth import *
+from gate_api import ApiClient, Configuration, Order, SpotApi
+
+client = load_gateio_creds('auth/auth.yml')
+spot_api = SpotApi(ApiClient(client))
+
+
+def get_last_price(base,quote):
+    """
+    Args:
+    'DOT', 'USDT'
+    """
+    tickers = spot_api.list_tickers(currency_pair=f'{base}_{quote}')
+    assert len(tickers) == 1
+    return tickers[0].last
+
+
+def get_min_amount(base,quote):
+    """
+    Args:
+    'DOT', 'USDT'
+    """
+    try:
+        min_amount = spot_api.get_currency_pair(currency_pair=f'{base}_{quote}').min_quote_amount
+    except Exception as e:
+        logger.error(e)
+    else:
+        return min_amount
+
+
+def place_order(base,quote, amount, side, last_price):
+    """
+    Args:
+    'DOT', 'USDT', 50, 'buy', 400
+    """
+    try:
+        order = Order(amount=str(float(amount)/float(last_price)), price=last_price, side=side, currency_pair=f'{base}_{quote}')
+        order = spot_api.create_order(order)
+    except Exception as e:
+        logger.error(e)
+        raise
+
+    else:
+        return order
